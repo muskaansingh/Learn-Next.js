f3ca4403f81d59053aeb8d7205532d5b0cf04f94
Muskaan Singh <muskaansingh235@gmail.com>
2021-03-24 11:21:41 +0530

    Added Navbar

--- next-basics/Components/Layout.js
+++ next-basics/Components/Layout.js
@@ -1,13 +1,17 @@
 import styles from '../styles/Layout.module.css';
+import Nav from '../Components/Navbar';
 
 const Layout = ({children}) => {
     return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-            <h1>Welcome to the homepage!</h1>
-                {children}
-            </main>
-        </div>
+        <>
+            <Nav/>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                {/* <h1>Welcome to the homepage!</h1> */}
+                    {children}
+                </main>
+            </div>
+        </>
     )
 }
 
