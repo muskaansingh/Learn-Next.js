b02260e64a851c3288a10ae4743c9261d1309952
Muskaan Singh <muskaansingh235@gmail.com>
2021-03-25 11:33:58 +0530

    Adding getStaticprops function for data fetching

--- next-basics/pages/index.js
+++ next-basics/pages/index.js
@@ -14,3 +14,16 @@ export default function Home() {
     </div>
   )
 }
+
+//Data Fetching!
+
+export const getStaticProps = async() => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10`)
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
+}
