bf39b79da8bbba9b50321e7dfea737e007e28e5d
Muskaan Singh <47673038+muskaansingh@users.noreply.github.com>
2021-03-27 21:24:18 +0530

    Update about.js

--- next-basics/pages/about.js
+++ next-basics/pages/about.js
@@ -1,17 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 const about = () => {
     return (
         <div>
             <Head>
                 <title>
-                    About!
+                    About-Page!
                 </title>
             </Head>
             <h1>Welcome to about page!</h1>
+            <p> Hello World! </p>
         </div>
     )
 }
 
-export default about
+export default about;
 
